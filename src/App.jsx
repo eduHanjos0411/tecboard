5852fd2e1af0d62b7154709817a989e83a823568
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-11-17 13:14:24 -0300

    style: movendo estilos do banner para componente separado; feat: criando lógica de listas para renderizar card de eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,65 @@
 import './App.css'
-import {FormularioDeEvento} from './components/FormularioDeEvento'
+import { Banner } from './components/Banner'
+import { FormularioDeEvento } from './components/FormularioDeEvento'
+import { Tema } from './components/Tema'
 
 function App() {
+
+  const temas = [
+    {
+      id: 1,
+      nome: "front-end"
+    },
+    {
+      id: 2,
+      nome: "back-end"
+    },
+    {
+      id: 3,
+      nome: "devops"
+    },
+    {
+      id: 4,
+      nome: "inteligência artificial"
+    },
+    {
+      id: 5,
+      nome: "data science"
+    },
+    {
+      id: 6,
+      nome: "cloud"
+    },
+  ]
+
+  const eventos = [
+    {
+      capa: 'https://raw.githubusercontent.com/viniciosneves/tecboard-assets/refs/heads/main/imagem_1.png',
+      tema: temas[0],
+      data: new Date(),
+      titulo: "Mulheres no front"
+    }
+  ]
 
   return (
     <main>
       <header>
         <img src="logoTecboard.png" alt="" />
-        </header>
-      <section>
-        <img src="BannerTecboar.png" alt="" />
-      </section>
-      <FormularioDeEvento/>
+      </header>
+
+      <Banner />
+
+      <FormularioDeEvento />
+
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item} />
+            <CardEvento evento={eventos}/>
+          </section>
+        )
+      })}
+
     </main>
   )
 }

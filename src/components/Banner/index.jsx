1cf6ad594f8333a41083839e452c1931e554df8d
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-11-17 13:11:48 -0300

    refactor: componentizando banner

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,9 @@
+import './bannerEstilos.css'
+
+export function Banner() {
+  return  (
+          <section className='banner'>
+            <img src="BannerTecboar.png" alt="" />
+          </section>
+  )
+}

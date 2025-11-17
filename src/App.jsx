8082279ea9a8182006423a4beb4ce0577ed56ea9
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-11-17 11:44:59 -0300

    feat: criando main

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import {FormularioDeEvento} from './components/FormularioDeEvento'
+
+function App() {
+
+  return (
+    <main>
+      <header>
+        <img src="logoTecboard.png" alt="" />
+        </header>
+      <section>
+        <img src="BannerTecboar.png" alt="" />
+      </section>
+      <FormularioDeEvento/>
+    </main>
+  )
+}
+
+export default App
